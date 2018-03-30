9ac73f9ec058f5feb80208b2448c12d6f60c9af0
Lucy Wall <lhwall@gmail.com>
2018-03-30 18:37:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
       if @discount != nil
       @total *= ((100.0 - discount.to_f)/100.0)
       "After the discount, the total comes to $#{total.to_i}."
-      else 
+      else "There is no discount to apply."
       end   
   end
   
