5609ecdd4d9680a20dee80421df96759097268d9
Lucy Wall <lhwall@gmail.com>
2018-03-30 17:49:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,9 @@
 class CashRegister 
-  attr_accessor :cash_register, :total
+  attr_accessor :cash_register, :total, :discount
   
   def initialize(discount = nil)
     @total = 0 
-    
+    @discount = discount
   end
   
   
