7acf192390210401624133ce7ea7f2565ac7fdd1
Lucy Wall <lhwall@gmail.com>
2018-03-30 18:57:58 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(item, amount, quantity = 1)
       @total += amount * quantity
       item_array = 
-     do  @items.push() << item
+      quantity.times @items.push() << item
   end  
     
   def apply_discount
